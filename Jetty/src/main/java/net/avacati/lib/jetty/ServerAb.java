c0fa66e687fec382ddc9289a44e39425614881ea
Torbjørn Søiland <tsoiland@avacati.net>
2015-08-23 15:52:31 +0200

    Initial commit with jetty lib module.

---
+++ Jetty/src/main/java/net/avacati/lib/jetty/ServerAb.java
@@ -0,0 +1,32 @@
+package net.avacati.lib.jetty;
+
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+
+import javax.servlet.Servlet;
+
+
+public class ServerAb {
+    private final Servlet servlet;
+    private final int port;
+
+    public ServerAb(Servlet servlet, int port) {
+        this.servlet = servlet;
+        this.port = port;
+    }
+
+    public void StartServer() throws Exception {
+        // Create server
+        Server server = new Server(port);
+
+        // Register servlet
+        ServletContextHandler handler = new ServletContextHandler();
+        handler.addServlet(new ServletHolder(servlet), "/");
+        server.setHandler(handler);
+
+        // Start
+        server.start();
+        server.join();
+    }
+}
