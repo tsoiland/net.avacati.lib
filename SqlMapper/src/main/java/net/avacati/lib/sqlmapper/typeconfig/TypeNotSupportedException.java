ee1d778ec04a3c976d71deb289c7b70a9a15d0d9
Torbjørn Søiland <tsoiland@avacati.net>
2016-07-02 18:27:09 +0200

    Deleted sql mapper. Moved to documentorm repo.

--- SqlMapper/src/main/java/net/avacati/lib/sqlmapper/typeconfig/TypeNotSupportedException.java
+++
@@ -1,7 +0,0 @@
-package net.avacati.lib.sqlmapper.typeconfig;
-
-public class TypeNotSupportedException extends RuntimeException{
-    TypeNotSupportedException(Class<?> type) {
-        super("No type mapping for: " + type.toString());
-    }
-}
